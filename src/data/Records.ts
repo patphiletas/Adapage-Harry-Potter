6b30a840bca9e6952ea7adee175a141fbda9bc23
Patrice Philetas <patricephiletas@Host-001.lan>
2026-02-24 21:22:00 +0100

    correction images

--- src/data/Records.ts
+++ src/data/Records.ts
@@ -7,7 +7,7 @@ export const records: RecordItem[] = [
     date: "31/10/1981",
     description:
       "À l’âge d’un an, Harry Potter devient le seul sorcier connu à avoir survécu directement au sortilège de mort Avada Kedavra. Lancé par Lord Voldemort lors de l’attaque de Godric’s Hollow, le sort est neutralisé par le sacrifice de Lily Potter, provoquant un rebond magique inédit. Cet événement marque la chute temporaire de Voldemort et constitue un cas unique dans l’histoire de la magie.",
-    photo: "/src/assets/images/records/Kedavra.jpeg"
+    photo: "/images/records/Kedavra.jpeg"
   },
   {
     id: "2",
@@ -15,7 +15,7 @@ export const records: RecordItem[] = [
     date: "1991-1992",
     description:
       "À seulement 11 ans, Harry Potter devient le plus jeune Attrapeur de Gryffondor depuis plus d’un siècle. Lors de son tout premier match officiel de Quidditch, il attrape le Vif d’Or, mettant fin à la rencontre et assurant la victoire de son équipe. Ce fait d’armes précoce établit un record de précocité sportive à Poudlard.",
-    photo: "/src/assets/images/records/quidditch_first.jpg"
+    photo: "/images/records/quidditch_first.jpg"
   },
   {
     id: "3",
@@ -23,7 +23,7 @@ export const records: RecordItem[] = [
     date: "1994-1995",
     description:
       "Sélectionné malgré lui par la Coupe de Feu, Harry Potter devient à 14 ans le plus jeune champion de l’histoire du Tournoi des Trois Sorciers. Confronté à des épreuves normalement réservées à des sorciers majeurs, il survit à un dragon, à une épreuve sous-marine et à un labyrinthe enchanté, remportant le tournoi au terme d’une compétition exceptionnellement dangereuse.",
-    photo: "/src/assets/images/records/trois_sorciers.jpg"
+    photo: "/images/records/trois_sorciers.jpg"
   },
   {
     id: "4",
@@ -31,6 +31,6 @@ export const records: RecordItem[] = [
     date: "1995-1996",
     description:
       "À l’âge de 15 ans, Harry Potter réussit à produire un Patronus corporel complet, une forme de magie défensive extrêmement avancée. Capable de repousser plusieurs Détraqueurs simultanément, cet exploit est rarissime pour un élève de Poudlard et généralement réservé à des sorciers adultes expérimentés, ce qui établit un record de maîtrise précoce.",
-    photo: "/src/assets/images/records/patronus.jpg"
+    photo: "/images/records/patronus.jpg"
   }
 ];
